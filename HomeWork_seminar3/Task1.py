c0240774dffdd691a178bcda5640dc992aeb93e0
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-11-30 16:52:32 +0700

    Задача 1

---
+++ HomeWork_seminar3/Task1.py
@@ -0,0 +1,19 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму
+# элементов списка, стоящих на нечётной позиции.
+# Пример: [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+from random import randint
+
+amount_list_elements = int(input('Введите количество элементов списка - '))
+diapason_start = int(input('Диапазон случайных чисел от - '))
+diapason_end = int(input('Диапазон случайных чисел до - '))
+list = []
+
+for i in range(amount_list_elements):
+    list.append(randint(diapason_start, diapason_end))
+print('Список из случайных чисел - ', list)
+
+count = 0
+for i in range(len(list)):
+    if i % 2 != 0:
+        count += list[i]
+print('Сумма элементов на нечетных позициях списка = ', count)
