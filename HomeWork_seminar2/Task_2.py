9035bad712406244e6e5bd9ecb39ec7f668956d5
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-11-30 22:37:34 +0700

    Задача 2

--- HomeWork_seminar2/Task_2.py
+++ HomeWork_seminar2/Task_2.py
@@ -4,17 +4,20 @@
 
 n = int(input('Введите количество элементов списка '))
 
+
 def Spisok(n):
     list = []
     for i in range(1, n + 1):
-        list.append((1 + 1/i) ** i)
+        list.append((1 + 1 / i) ** i)
     return list
+
 
 def Sum(list):
     sum = 0
     for i in range(len(list)):
         sum = sum + list[i]
     return sum
+
 
 list = Spisok(n)
 print(list)
