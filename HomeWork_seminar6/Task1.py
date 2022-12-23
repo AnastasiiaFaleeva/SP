39a74f561975799c72055919c0e84d64396aa138
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-12-23 14:21:48 +0700

    Задача 1

---
+++ HomeWork_seminar6/Task1.py
@@ -0,0 +1,20 @@
+# Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр.
+# Пример:
+# 6782 -> 23
+# 0,56 -> 11
+
+# num = input('Введите число - ')
+#
+# def SumNumbers(number):
+#     number = number.replace('.', '')
+#     number = number.replace(',', '')
+#     summa = 0
+#     for i in number:
+#         summa = summa + int(i)
+#     return summa
+#
+#
+# print('Сумма цифр в числе', num, '=', SumNumbers(num))
+
+print(sum(list(map(lambda x: 0 if x == '.' else int(x), input('введите  число: ')))))
